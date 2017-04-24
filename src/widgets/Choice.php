273d83222e68d05c283979aa7801edc8ece88a0f
Archenkov Pavel <Deadly299@gmail.com>
2017-04-24 10:20:43 +0500

    Added null validation

--- src/widgets/Choice.php
+++ src/widgets/Choice.php
@@ -28,7 +28,9 @@ class Choice extends \yii\base\Widget
             if(!is_null($this->includeId)) {
                 if(!isset($this->includeId[$filter->id]))continue;
             }
-            if(isset($this->excludeId[$filter->id]))continue;
+            if(!is_null($this->excludeId)) {
+                if (isset($this->excludeId[$filter->id])) continue;
+            }
             $row = $this->renderFilter($filter);
             $return[] = Html::tag('div', implode('', $row), ['class' => ' panel panel-default']);
         }
